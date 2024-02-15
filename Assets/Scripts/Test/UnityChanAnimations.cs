35806b9e97e837af65c9ac943b5d04a2f70e7cba
LB-Bernard <bernard.panares@limitbreakstudio.com>
2024-02-15 11:46:34 +0800

    Added idle variations and tier animations
    Added idle variations, tier animations, and proper reactions and sounds

---
+++ Assets/Scripts/Test/UnityChanAnimations.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using UnityEngine;
+
+public class UnityChanAnimations : MonoBehaviour
+{
+    [SerializeField] private Animator animator = null;
+    [SerializeField] private int minInterval = 3;
+    [SerializeField] private int maxInterval = 5;
+    private bool isMPAPlaying = false;
+
+    private void Start()
+    {
+        StartCoroutine(RandomizeStates());
+    }
+
+    private IEnumerator RandomizeStates()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(Random.Range(minInterval, maxInterval));
+
+            if (!isMPAPlaying)
+            {
+                int random = Random.Range(1, 5);
+                animator.SetInteger("State", random);
+                animator.SetTrigger("PlayState");
+            }
+        }
+    }
+
+    public void SetMPAState(bool isPlaying)
+    {
+        isMPAPlaying = isPlaying;
+
+        if (isMPAPlaying)
+        {
+            animator.SetInteger("State", 0);
+            animator.SetTrigger("PlayState");
+        }
+    }
+}
